379d7e2d94136e20e3d2ce2de5568b7ccdf0f818
Antonis Papaioannou <mrantonp@gmail.com>
2024-08-01 11:40:40 +0300

    create threadCard

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -13,8 +13,7 @@ interface Params {
   path: string,
 }
 
-export async function createThread({ text, author, communityId, path }: Params
-) {
+export async function createThread({ text, author, communityId, path }: Params) {
   try {
     
     connectToDB();
@@ -47,7 +46,34 @@ export async function createThread({ text, author, communityId, path }: Params
   } catch (error) {
     console.log(error);
     throw new Error("Failed to create thread");
-  }
-  
-  
+  } 
+}
+
+
+export async function fetchPosts(pageNumber = 1, pageSize = 20) {
+  connectToDB();
+
+  const skipAmount = (pageNumber - 1) * pageSize;
+
+  const postsQuery = Thread.find({ parentId: { $in: [null, undefined] } })
+    .sort({ createdAt: 'desc' })
+    .skip(skipAmount)
+    .limit(pageSize)
+    .populate({path: 'author', model: User})
+    .populate({
+      path: 'children',
+      populate: {
+        path: 'author',
+        model: User,
+        select: "_id name parentId image"
+      }
+    });
+
+    const totalPostsCount = await Thread.countDocuments({ parentId: { $in: [null, undefined] } });
+
+    const posts = await postsQuery.exec();
+
+    const isNext = totalPostsCount > skipAmount + posts.length;
+
+    return { posts, isNext };
 }
