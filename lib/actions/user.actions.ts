bed8596868ce983ed06d603edf65d767f8488039
Antonis Papaioannou <mrantonp@gmail.com>
2024-08-02 12:36:52 +0300

    create community

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -145,3 +145,30 @@ export async function fetchUsers({
     
   }
 }
+
+export async function getActivity(userId: string) {
+  try {
+    connectToDB();
+
+    const userThreads = await Thread.find({ author: userId });
+
+    const childThreadIds = userThreads.reduce((acc, userThread) => {
+      return acc.concat(userThread.children);
+    }, []);
+
+    
+    const replies = await Thread.find({
+      _id: { $in: childThreadIds },
+      author: { $ne: userId }, // Exclude threads authored by the same user
+    }).populate({
+      path: "author",
+      model: User,
+      select: "name image _id",
+    });
+
+    return replies;
+  } catch (error) {
+    console.error("Error fetching replies: ", error);
+    throw error;
+  }
+}
